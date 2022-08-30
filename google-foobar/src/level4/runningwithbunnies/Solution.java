2d76b6648efad9da82cc6b314a1ad613a93c2120
Pablo Albaladejo <pablo@hireful.co.uk>
2022-08-30 15:57:37 +0200

    chore: Graph scaffolding

--- google-foobar/src/level4/runningwithbunnies/Solution.java
+++ google-foobar/src/level4/runningwithbunnies/Solution.java
@@ -1,9 +1,62 @@
 package level4.runningwithbunnies;
-//https://en.wikipedia.org/wiki/Bellman%E2%80%93Ford_algorithm
-//https://www.javatpoint.com/bellman-ford-algorithm-java
-//https://www.youtube.com/watch?v=wBhviRc78Zo
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+class Edge {
+    int origin;
+    int destination;
+    int weight;
+
+    public Edge(int origin, int destination, int weight) {
+        this.origin = origin;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj.toString().equals(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Edge(" + this.origin + ", " + this.destination + ", " + this.weight + ")";
+    }
+}
+
+class Graph{
+    int[] vertices;
+    ArrayList<Edge> edges;
+
+    public Graph(int[][] input){
+        this.vertices = getVertices(input);
+        this.edges = getEdges(input);
+    }
+
+    public int[] bellmanFord(){
+        return new int[]{};
+    }
+
+    public static int[] getVertices(int[][] input) {
+        return IntStream.rangeClosed(0, input[0].length - 1).toArray();
+    }
+
+    public static ArrayList<Edge> getEdges(int[][] input) {
+        ArrayList<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < input[0].length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                if (i != j && input[i][j] != Integer.MAX_VALUE) edges.add(new Edge(i, j, input[i][j]));
+            }
+        }
+        return edges;
+    }
+}
+
 public class Solution {
     public static int[] solution(int[][] times, int times_limit) {
-        return new int[]{0,1};
+        Graph graph = new Graph(times);
+        return new int[]{0, 1};
     }
 }
--- google-foobar/src/level4/runningwithbunnies/SolutionTestHelpers.java
+++ google-foobar/src/level4/runningwithbunnies/SolutionTestHelpers.java
@@ -1,4 +1,71 @@
 package level4.runningwithbunnies;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
 public class SolutionTestHelpers {
+    @Test
+    public void getVertices() {
+        int[][] matrix = {{0, 1, 1, 1, 1}, {1, 0, 1, 1, 1}, {1, 1, 0, 1, 1}, {1, 1, 1, 0, 1}, {1, 1, 1, 1, 0}};
+        int[] vertices = Graph.getVertices(matrix);
+        assertArrayEquals(new int[]{0, 1, 2, 3, 4}, vertices);
+    }
+
+    @Test
+    public void getEdges() {
+        int[][] matrix = {{0, 1, 1}, {2, 0, 2}, {3, 3, 0}};
+        ArrayList<Edge> edges = Graph.getEdges(matrix);
+
+        ArrayList<Edge> expected = new ArrayList<>();
+        expected.add(new Edge(0, 1, 1));
+        expected.add(new Edge(0, 2, 1));
+        expected.add(new Edge(1, 0, 2));
+        expected.add(new Edge(1, 2, 2));
+        expected.add(new Edge(2, 0, 3));
+        expected.add(new Edge(2, 1, 3));
+
+        assertIterableEquals(expected, edges);
+    }
+    @Test
+    public void getEdgesInf() {
+        int[][] matrix = {
+                {0, -1, 4, Integer.MAX_VALUE, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, 0, 3, 2, 2},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, Integer.MAX_VALUE, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, 1, 5, 0, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, -3, 0}
+        };
+        ArrayList<Edge> edges = Graph.getEdges(matrix);
+
+        ArrayList<Edge> expected = new ArrayList<>();
+        expected.add(new Edge(0, 1, -1));
+        expected.add(new Edge(0, 2, 4));
+        expected.add(new Edge(1, 2, 3));
+        expected.add(new Edge(1, 3, 2));
+        expected.add(new Edge(1, 4, 2));
+        expected.add(new Edge(3, 1, 1));
+        expected.add(new Edge(3, 2, 5));
+        expected.add(new Edge(4, 3, -3));
+
+        assertIterableEquals(expected, edges);
+    }
+
+    @Test
+    public void bellmanFord() {
+        int[][] matrix = {
+                {0, -1, 4, Integer.MAX_VALUE, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, 0, 3, 2, 2},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, 0, Integer.MAX_VALUE, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, 1, 5, 0, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, -3, 0}
+        };
+        Graph graph = new Graph(matrix);
+        int[] distances = graph.bellmanFord();
+        assertArrayEquals(new int[]{0, -1, 2, -2, 1}, distances);
+    }
+
+
 }
