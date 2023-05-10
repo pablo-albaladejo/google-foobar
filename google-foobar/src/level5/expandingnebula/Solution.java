40251e98a7dbd4844c9e48d27d9dea60588f0070
Pablo Albaladejo <pablo@hireful.co.uk>
2023-05-10 13:52:42 +0200

    chore: expanding nebula solved

--- google-foobar/src/level5/expandingnebula/Solution.java
+++ google-foobar/src/level5/expandingnebula/Solution.java
@@ -1,7 +1,90 @@
 package level5.expandingnebula;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Solution {
-    public static int solution (boolean[][] g){
-        return Integer.MAX_VALUE;
+    public static int generate(int c1, int c2, int bitlen) {
+        int a = c1 & ~(1 << bitlen);
+        int b = c2 & ~(1 << bitlen);
+        int c = c1 >> 1;
+        int d = c2 >> 1;
+        return (a & ~b & ~c & ~d) | (~a & b & ~c & ~d) | (~a & ~b & c & ~d) | (~a & ~b & ~c & d);
+    }
+
+    public static Map<List<Integer>, Set<Integer>> buildMap(int n, List<Integer> nums) {
+        Map<List<Integer>, Set<Integer>> mapping = new HashMap<>();
+        Set<Integer> numSet = new HashSet<>(nums);
+        for (int i = 0; i < (1 << (n + 1)); i++) {
+            for (int j = 0; j < (1 << (n + 1)); j++) {
+                int generation = generate(i, j, n);
+                if (numSet.contains(generation)) {
+                    List<Integer> key = new ArrayList<>();
+                    key.add(generation);
+                    key.add(i);
+                    mapping.computeIfAbsent(key, k -> new HashSet<>()).add(j);
+                }
+            }
+        }
+        return mapping;
+    }
+
+    public static boolean[][] transpose(boolean[][] matrix) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+        boolean[][] transposedMatrix = new boolean[numCols][numRows];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposedMatrix;
+    }
+
+    public static int solution(boolean[][] g) {
+        boolean[][] matrix = transpose(g);
+        int nrows = matrix.length;
+        int ncols = matrix[0].length;
+
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < nrows; i++) {
+            int num = 0;
+            for (int j = 0; j < ncols; j++) {
+                num += (matrix[i][j] ? (1 << j) : 0);
+            }
+            nums.add(num);
+        }
+
+        Map<List<Integer>, Set<Integer>> mapping = buildMap(ncols, nums);
+
+        Map<Integer, Integer> preimage = new HashMap<>();
+        for (int i = 0; i < (1<<(ncols+1)); i++) {
+            preimage.put(i, 1);
+        }
+
+        for (int row : nums) {
+            Map<Integer, Integer> nextRow = new HashMap<>();
+            for (int c1 : preimage.keySet()) {
+                for (int c2 : mapping.getOrDefault(Arrays.asList(row, c1), new HashSet<>())) {
+                    int count = nextRow.getOrDefault(c2, 0);
+                    nextRow.put(c2, count + preimage.get(c1));
+                }
+            }
+            preimage = nextRow;
+        }
+
+        int sum = 0;
+        for (int count : preimage.values()) {
+            sum += count;
+        }
+        return  sum;
     }
 }
+
+
--- google-foobar/src/level5/expandingnebula/SolutionTest.java
+++ google-foobar/src/level5/expandingnebula/SolutionTest.java
@@ -2,9 +2,83 @@ package level5.expandingnebula;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
+
+    @Test
+    public void testCaseGenerate055() {
+        assertEquals(7, Solution.generate(0, 5, 5));
+    }
+
+    @Test
+    public void testCaseGenerate0105() {
+        assertEquals(15, Solution.generate(0, 10, 5));
+    }
+
+    @Test
+    public void testCaseGenerate105() {
+        assertEquals(1, Solution.generate(1, 0, 5));
+    }
+
+    @Test
+    public void testCaseGenerate1115() {
+        assertEquals(14, Solution.generate(1, 11, 5));
+    }
+
+    @Test
+    public void testCaseGenerate1165() {
+        assertEquals(25, Solution.generate(1, 16, 5));
+    }
+
+    @Test
+    public void testCaseBuildMap11() {
+        List<Integer> nums = Arrays.asList(1);
+        Map<List<Integer>, Set<Integer>> result = Solution.buildMap(1, nums);
+
+        Map<List<Integer>, Set<Integer>> expected = Stream.of(new Object[][]{
+                {Arrays.asList(1, 0), Stream.of(1, 2).collect(Collectors.toSet())},
+                {Arrays.asList(1, 1), Stream.of(0).collect(Collectors.toSet())},
+                {Arrays.asList(1, 2), Stream.of(0).collect(Collectors.toSet())},
+        }).collect(Collectors.toMap(data -> (List<Integer>) data[0], data -> (Set<Integer>) data[1]));
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testCaseBuildMap12() {
+        List<Integer> nums = Arrays.asList(2);
+        Map<List<Integer>, Set<Integer>> result = Solution.buildMap(1, nums);
+
+        Map<List<Integer>, Set<Integer>> expected = Stream.of(new Object[][]{
+        }).collect(Collectors.toMap(data -> (List<Integer>) data[0], data -> (Set<Integer>) data[1]));
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testCaseBuildMap21() {
+        List<Integer> nums = Arrays.asList(1);
+        Map<List<Integer>, Set<Integer>> result = Solution.buildMap(2, nums);
+
+        Map<List<Integer>, Set<Integer>> expected = Stream.of(new Object[][]{
+                {Arrays.asList(1, 0), Stream.of(1, 6).collect(Collectors.toSet())},
+                {Arrays.asList(1, 1), Stream.of(0).collect(Collectors.toSet())},
+                {Arrays.asList(1, 2), Stream.of(4).collect(Collectors.toSet())},
+                {Arrays.asList(1, 4), Stream.of(2, 5, 6).collect(Collectors.toSet())},
+                {Arrays.asList(1, 5), Stream.of(4).collect(Collectors.toSet())},
+                {Arrays.asList(1, 6), Stream.of(0, 4).collect(Collectors.toSet())},
+        }).collect(Collectors.toMap(data -> (List<Integer>) data[0], data -> (Set<Integer>) data[1]));
+
+        assertEquals(expected, result);
+    }
 
     @Test
     public void testCase1() {
